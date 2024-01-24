87d83ad1b565796b49762c1c4958fbefd295089f
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2024-01-24 15:01:02 +0100

    chore: format

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 import os
+
 from conan import ConanFile
 from conan.tools.files import replace_in_file, rm
 from conan.tools.gnu import Autotools
@@ -17,7 +18,9 @@ class StringUtilConan(ConanFile):
     generators = "AutotoolsToolchain"
 
     def build(self):
-        replace_in_file(self, "project.properties", "VERSION=1.1.7", f"VERSION={self.version.replace('.', ':').replace('-', '')}")
+        replace_in_file(
+            self, "project.properties", "VERSION=1.1.7", f"VERSION={self.version.replace('.', ':').replace('-', '')}"
+        )
         self.run("autoreconf -if")
         autotools = Autotools(self)
         autotools.fpic = self.options.fPIC
@@ -33,12 +36,15 @@ class StringUtilConan(ConanFile):
         if os.getenv("BUILD_MODE") == "cross" or os.getenv("CHOST"):
             # we are cross compiling,
             # just test whether test-program compiles
-            not_working = ("x86_64-w64-mingw32", "i686-w64-mingw32") # those are hosts where somehow autotools fails to provide include path as requested in Makefile.am
+            not_working = (
+                "x86_64-w64-mingw32",
+                "i686-w64-mingw32",
+            )  # those are hosts where somehow autotools fails to provide include path as requested in Makefile.am
             if os.getenv("CHOST") not in not_working:
                 autotools.make(target="example", args=["-C", "test/example", "V=1"])
         else:
             autotools.make(target="check", args=["VERBOSE=1"])
-        
+
     def package(self):
         autotools = Autotools(self)
         autotools.install()
@@ -47,6 +53,6 @@ class StringUtilConan(ConanFile):
                 rm(self, os.path.join(self.package_folder, file))
 
     def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.libdirs = ['lib', 'lib64']
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.libdirs = ["lib", "lib64"]
         self.cpp_info.libs = ["string_util"]
